cdac13259f16baa9b052fd4766ecbf50bdb16fcd
minsuk1003 <kms1003go@khu.ac.kr>
2024-07-01 23:00:37 +0900

    07.01 "statiz crawling"

--- statiz.py
+++ statiz.py
@@ -3,18 +3,5 @@ from bs4 import BeautifulSoup
 import pandas as pd
 from tqdm import tqdm
 
-url = 'https://statiz.sporki.com/' + '/player/?m=playerinfo&p_no=11190' + '&m=income'
-
-response = requests.get(url)
-soup = BeautifulSoup(response.text, 'html.parser')
-
-cols = soup.find_all('th')
-data = soup.find_all('td')
-
-# 리스트를 청크로 나누는 함수
-def chunk_list(lst, chunk_size):
-    return [lst[i:i + chunk_size] for i in range(0, len(lst), chunk_size)]
-
-salary_data = chunk_list([batter.text for batter in data], 3)
-
-print(pd.DataFrame(salary_data, columns=['Year', '연봉(만원)', 'WAR']))
+columns = ['Year', 'Team', 'Rank', 'Name', 'Pos', 'WAR', 'oWAR', 'dWAR', 'G', 'PA', 'ePA', 'AB', 'R', 'H', '2B', '3B', 'HR', 'TB', 'RBI', 'SB', 'CS', 'BB', 'HP', 'IB', 'SO', 'GDP', 'SH', 'SF', 'AVG', 'OBP', 'SLG', 'OPS', 'R/ePA', 'wRC+', 'WAR2']
+print(len(columns))
--- statiz_crawling.py
+++ statiz_crawling.py
@@ -96,6 +96,53 @@ def collect_seasonal_data(year_list, team_dict):
     pitcher_df = pitcher_df.drop('WAR2', axis=1)
     return batter_df, pitcher_df
 
+# 특정 연도와 팀의 데이터를 가져오는 함수
+def fetch_batter_game_data(seasonal_df):
+    df = None
+    for i in tqdm(range(len(seasonal_df))):
+        url = f"https://statiz.sporki.com/{seasonal_df.loc[i,'url']}&year={seasonal_df.loc[i,'Year']}&m=day"
+        response = requests.get(url)
+        soup = BeautifulSoup(response.text, 'html.parser')
+    
+        columns = ['Year', 'Team', 'Name', 'Date', 'Opponent', 'Result', 'Order', 'Pos', 'GS', 'ePA', 'AB', 'R', 'H', '2B', '3B', 'HR', 'TB', 'RBI', 'SB', 'CS', 'BB', 'HP', 'IB', 'SO', 'GDP', 'SH', 'SF', 'accumulated_AVG', 'accumulated_OBP', 'accumulated_SLG', 'accumulated_OPS', 'NP', 'avLI', 'RE24', 'WPA']
+
+        batter_datas = soup.find_all('td')
+        chunked_batter_data = chunk_list([batter.text for batter in batter_datas], len(columns)-3)
+        
+        new_elements = [seasonal_df.loc[i,'Year'], seasonal_df.loc[i, 'Team'], seasonal_df.loc[i, 'Name']]
+        batter_data = add_elements_to_sublist(chunked_batter_data, new_elements)
+        
+        new_df = pd.DataFrame(batter_data, columns=columns)
+        if df is None:
+            df = new_df
+        else:
+            df = pd.concat([df, new_df], ignore_index=True)
+    return df
+
+# 특정 연도와 팀의 데이터를 가져오는 함수
+def fetch_pitcher_game_data(seasonal_df):
+    df = None
+    for i in tqdm(range(len(seasonal_df))):
+        url = f"https://statiz.sporki.com/{seasonal_df.loc[i,'url']}&year={seasonal_df.loc[i,'Year']}&m=day"
+        response = requests.get(url)
+        soup = BeautifulSoup(response.text, 'html.parser')
+    
+        columns = ['Year', 'Team', 'Name', 'Date', 'Opponent', 'Result', 'GS', 'IP', 'R', 'ER', 'rRA', 'TBF', 'AB', 'H', '2B', '3B', 'HR', 'BB', 'IB', 'HP', 'SO', 'NP', 'WHIP', 'AVG', 'OBP', 'OPS', 'ERA', 'avLI', 'RE24', 'WPA', 'GSC', 'DEC', 'Int']
+
+        pitcher_datas = soup.find_all('td')
+        chunked_pitcher_data = chunk_list([pitcher.text for pitcher in pitcher_datas], len(columns)-3)
+        
+        new_elements = [seasonal_df.loc[i,'Year'], seasonal_df.loc[i, 'Team'], seasonal_df.loc[i, 'Name']]
+        pitcher_data = add_elements_to_sublist(chunked_pitcher_data, new_elements)
+        
+        new_df = pd.DataFrame(pitcher_data, columns=columns)
+        if df is None:
+            df = new_df
+        else:
+            df = pd.concat([df, new_df], ignore_index=True)
+    return df
+
+
 def fetch_player_salary(url, team, name):
     response = requests.get(url)
     soup = BeautifulSoup(response.text, 'html.parser')
@@ -124,6 +171,8 @@ def collect_player_salary(seasonal_df):
 
 # 데이터 수집 실행
 seasonal_batter_df, seasonal_pitcher_df = collect_seasonal_data(year_list, team_dict)
+game_batter_df = fetch_batter_game_data(seasonal_batter_df)
+game_pitcher_df = fetch_pitcher_game_data(seasonal_pitcher_df)
 salary_df = collect_player_salary(pd.concat([seasonal_batter_df[['Name', 'Team', 'url']], seasonal_pitcher_df[['Name', 'Team', 'url']]]).drop_duplicates(subset='url', keep='first', ignore_index=True))
 
 # 데이터프레임 출력
@@ -131,8 +180,19 @@ print(seasonal_batter_df.iloc[:, :10])
 print(seasonal_batter_df.iloc[:, 10:20])
 print(seasonal_batter_df.iloc[:, 20:30])
 print(seasonal_batter_df.iloc[:, 30:])
+
 print(seasonal_pitcher_df.iloc[:, :10])
 print(seasonal_pitcher_df.iloc[:, 10:20])
 print(seasonal_pitcher_df.iloc[:, 20:30])
 print(seasonal_pitcher_df.iloc[:, 30:])
+
+print(game_batter_df.iloc[:, :10])
+print(game_batter_df.iloc[:, 10:20])
+print(game_batter_df.iloc[:, 20:30])
+print(game_batter_df.iloc[:, 30:])
+
+print(game_pitcher_df.iloc[:, :10])
+print(game_pitcher_df.iloc[:, 10:20])
+print(game_pitcher_df.iloc[:, 20:30])
+print(game_pitcher_df.iloc[:, 30:])
 print(salary_df)
